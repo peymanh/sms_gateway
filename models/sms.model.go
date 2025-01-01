fabc123b38c8bb26902a436b4bf48f54e0347188
Peyman Hassanabadi <hassanabadi.peyman@gmail.com>
2025-01-01 14:28:45 +0330

    models: adding status and error message to SMSLog model

--- models/sms.model.go
+++ models/sms.model.go
@@ -6,17 +6,27 @@ import (
 	"github.com/google/uuid"
 )
 
+type SMSResultStatus int
+
+const (
+	SMSResultPending SMSResultStatus = 0
+	SMSResultSuccess SMSResultStatus = 1
+	SMSResultFailed  SMSResultStatus = 2
+	SMSResultError   SMSResultStatus = 3
+)
+
 type SMSLog struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id,omitempty"`
-	Body      string    `gorm:"not null" json:"body,omitempty"`
-	Language  string    `gorm:"not null" json:"language,omitempty"`
-	Receiver  string    `gorm:"not null" json:"receiver,omitempty"`
-	Cost      int       `gorm:"not null" json:"cost,omitempty"`
-	UserID    uuid.UUID `gorm:"not null" json:"user_id,omitempty"`
-	User      User      `gorm:"constraint:OnUpdate:CASCADE;"`
-	Status    bool      `gorm:"not null" json:"status,omitempty"`
-	CreatedAt time.Time `gorm:"not null" json:"created_at,omitempty"`
-	UpdatedAt time.Time `gorm:"not null" json:"updated_at,omitempty"`
+	ID           uuid.UUID       `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id,omitempty"`
+	Body         string          `gorm:"not null" json:"body,omitempty"`
+	Language     string          `gorm:"not null" json:"language,omitempty"`
+	Receiver     string          `gorm:"not null" json:"receiver,omitempty"`
+	Cost         int             `gorm:"not null" json:"cost,omitempty"`
+	UserID       uuid.UUID       `gorm:"not null" json:"user_id,omitempty"`
+	User         User            `gorm:"constraint:OnUpdate:CASCADE;"`
+	Status       SMSResultStatus `gorm:"not null;default:0" json:"status,omitempty"`
+	ErrorMessage string          `gorm:"not null" json:"error_message,omitempty"`
+	CreatedAt    time.Time       `gorm:"not null" json:"created_at,omitempty"`
+	UpdatedAt    time.Time       `gorm:"not null" json:"updated_at,omitempty"`
 }
 
 type SendInput struct {
